c8b4a9a2291716f67a2d66ce58298a5531e94883
Ernandes R Souza <ernandesrsouza@gmail.com>
2025-03-08 18:31:48 -0400

    Shared: table cell merge class

--- resources/views/components/shared/table/cell.blade.php
+++ resources/views/components/shared/table/cell.blade.php
@@ -1,8 +1,11 @@
 <div
-    class="
-        table-cell {{ $headerCell ? 'px-3 py-4' : 'p-3' }}
-        border-b border-zinc-200 dark:border-zinc-800
-        text-zinc-500 dark:text-zinc-300">
+    {{ $attributes->merge([
+        'class' => implode(' ', [
+            'table-cell ' . ($headerCell ? 'px-3 py-4' : 'p-3'),
+            'border-b border-zinc-200 dark:border-zinc-800',
+            'text-zinc-500 dark:text-zinc-300',
+        ]),
+    ]) }}>
     @empty($value)
         {{ $slot }}
     @else
