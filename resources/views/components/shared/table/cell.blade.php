68af257982542963d86c10cbfc5affe5b3570417
Ernandes R Souza <ernandesrsouza@gmail.com>
2025-03-08 18:39:02 -0400

    Shared: table header cell font medium

--- resources/views/components/shared/table/cell.blade.php
+++ resources/views/components/shared/table/cell.blade.php
@@ -1,7 +1,7 @@
 <div
     {{ $attributes->merge([
         'class' => implode(' ', [
-            'table-cell ' . ($headerCell ? 'px-3 py-4' : 'p-3'),
+            'table-cell ' . ($headerCell ? 'px-3 py-4 font-medium' : 'p-3'),
             'border-b border-zinc-200 dark:border-zinc-800',
             'text-zinc-500 dark:text-zinc-300',
         ]),
