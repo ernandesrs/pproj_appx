2c560ac5aaf7c3522022530e39e44c1cc72edf8e
Ernandes R Souza <ernandesrsouza@gmail.com>
2025-03-09 09:55:05 -0400

    Shared: dropdown transition

--- resources/views/components/shared/dropdown.blade.php
+++ resources/views/components/shared/dropdown.blade.php
@@ -40,6 +40,14 @@
     <div
         x-show="visible"
 
+        x-transition:enter="duration-200 ease-in"
+        x-transition:enter-start="opacity-0 scale-90 -translate-y-1/12 translate-x-1/12 -skew-3"
+        x-transition:enter-end="opacity-100 scale-100 -translate-y-0 translate-x-0"
+
+        x-transition:leave="duration-200 ease-in-out"
+        x-transition:leave-start="opacity-100 scale-100 -translate-y-0 translate-x-0"
+        x-transition:leave-end="opacity-0 scale-90 -translate-y-1/12 translate-x-1/12 skew-3"
+
         {{ $attributes->merge([
             'class' => 'absolute ' . ['left' => 'left-0', 'right' => 'right-0'][$getLocation()],
         ]) }}
