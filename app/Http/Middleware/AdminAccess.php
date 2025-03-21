1830536daac7767ecb379c4557acf993cb7724c7
Ernandes R Souza <ernandesrsouza@gmail.com>
2025-03-21 09:09:44 -0400

    Admin: middelwar admin access

---
+++ app/Http/Middleware/AdminAccess.php
@@ -0,0 +1,28 @@
+<?php
+
+namespace App\Http\Middleware;
+
+use Closure;
+use Illuminate\Http\Request;
+use Symfony\Component\HttpFoundation\Response;
+
+class AdminAccess
+{
+    /**
+     * Handle an incoming request.
+     *
+     * @param  \Closure(\Illuminate\Http\Request): (\Symfony\Component\HttpFoundation\Response)  $next
+     */
+    public function handle(Request $request, Closure $next): Response
+    {
+        /**
+         * @var \App\Models\User
+         */
+        $user = $request->user();
+        if (!$user->hasAnyRole(\App\Enums\Permissions\Admin\RolesEnum::cases())) {
+            abort(401);
+        }
+
+        return $next($request);
+    }
+}
