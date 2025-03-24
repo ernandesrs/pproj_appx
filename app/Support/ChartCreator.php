6b25a72b985db02663ec711f5dcb73b98dae6f98
Ernandes R Souza <ernandesrsouza@gmail.com>
2025-03-23 20:32:40 -0400

    Shared: rendering chart line and area

--- app/Support/ChartCreator.php
+++ app/Support/ChartCreator.php
@@ -5,31 +5,120 @@ namespace App\Support;
 class ChartCreator
 {
     /**
-     * Chart type. Allows: line, area, column, pie and radialbar
-     * @var string
+     * Data
+     * @var array
      */
-    public string $type = 'line';
+    public array $data = [];
 
     /**
-     * Line type
+     * Constructor
+     */
+    public function __construct()
+    {
+        $this->data = [
+            'chart' => [
+                'type' => 'line',
+                'height' => null
+            ],
+            'series' => []
+        ];
+    }
+
+    /**
+     * Set Data
+     * @param string $type
+     * @param ?string $title
+     * @param ?string $chartHeight
      * @return ChartCreator
      */
-    public static function line(): ChartCreator
+    private static function setData(string $type, ?string $title = null, ?string $height = null): ChartCreator
     {
         $chart = new ChartCreator;
-        $chart->type = 'line';
+
+        $chart->data['chart']['type'] = $type;
+        $title ? $chart->data['title'] = [
+            'text' => $title,
+            'align' => 'left'
+        ] : null;
+        $height ? $chart->data['chart']['height'] = $height : null;
+
         return $chart;
     }
 
     /**
-     * Area type
+     * Line type
+     * @param ?string $title
+     * @param ?string $height
      * @return ChartCreator
      */
-    public static function area(): ChartCreator
+    public static function line(?string $title = null, ?string $height = null): ChartCreator
     {
-        $chart = new ChartCreator;
-        $chart->type = 'area';
-        return $chart;
+        return static::setData('line', $title, $height);
+    }
+
+    public static function area(?string $title = null, ?string $height = null): ChartCreator
+    {
+        return static::setData('area', $title, $height);
+    }
+
+    /**
+     * Add Vertical Info
+     * @param int $min
+     * @param int $max
+     * @param mixed $title
+     * @return ChartCreator
+     */
+    public function addVerticalInfo(int $min, int $max, ?string $title = null): ChartCreator
+    {
+        $this->data['yaxis']['min'] = $min;
+        $this->data['yaxis']['max'] = $max;
+
+        $title ? $this->data['yaxis']['title']['text'] = $title : null;
+
+        return $this;
+    }
+
+    /**
+     * Add Horizontal Info
+     * @param array $labels
+     * @param mixed $title
+     * @return ChartCreator
+     */
+    public function addHorizontalInfo(array $labels, ?string $title = null): ChartCreator
+    {
+        $this->data['xaxis']['type'] = 'category';
+        $this->data['xaxis']['categories'] = $labels;
+
+        $title ? $this->data['xaxis']['title']['text'] = $title : null;
+
+        return $this;
+    }
+
+    /**
+     * Add Serie Data
+     * https://apexcharts.com/docs/series/
+     * @param string $name
+     * @param array $serieData
+     *      Example 1: [20, 10]
+     *      Example 2: [[1, 34], [3, 54]]
+     *      Example 3: [
+     *          ['x' => 1, 'y' => 34],
+     *          ['x' => 3, 'y' => 54]
+     *      ]
+     *      Example 4: [
+     *          ['x' => 'Apple', 'y' => 34],
+     *          ['x' => 'Orange, 'y' => 54]
+     *      ]
+     *
+     * @return ChartCreator
+     */
+    public function addSerieData(string $name, array $serieData = []): ChartCreator
+    {
+        $this->data['series'][] = [
+            'name' => $name,
+            'data' => $serieData
+        ];
+        return $this;
     }
 
     /**
@@ -38,8 +127,9 @@ class ChartCreator
      */
     public function toArray(): array
     {
-        return [
-            'type' => $this->type
+        $this->data['stroke'] = [
+            'curve' => 'smooth'
         ];
+        return $this->data;
     }
 }
--- resources/views/components/shared/charts/chart.blade.php
+++ resources/views/components/shared/charts/chart.blade.php
@@ -4,24 +4,9 @@
     chartInstance: null,
 
     init() {
-        var options = {
-            chart: {
-                type: this.configuration.type
-            },
-            series: [{
-                name: 'sales',
-                data: [30, 40, 35, 50, 49, 60, 70, 91, 125]
-            }],
-            xaxis: {
-                categories: [1991, 1992, 1993, 1994, 1995, 1996, 1997, 1998, 1999]
-            }
-        };
-
-        this.chartInstance = new window['apexCharts']($el, options);
-
+        this.chartInstance = new window['apexCharts']($el, this.configuration);
         this.chartInstance.render();
     }
 }"
     {{ $attributes }}>
-    <!-- When there is no desire, all things are at peace. - Laozi -->
 </div>
