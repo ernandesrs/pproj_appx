99a247953a2724652f03e997df1f967cda788858
Ernandes R Souza <ernandesrsouza@gmail.com>
2025-03-25 19:55:46 -0400

    Chart formatters

--- resources/views/components/shared/charts/chart.blade.php
+++ resources/views/components/shared/charts/chart.blade.php
@@ -12,14 +12,49 @@
                 ...this.configuration
             });
 
+            this.setConfigurations();
             this.setColors();
-
             this.chartInstance.render();
         },
 
         themeChange(event) {
             this.setColors(event.detail?.theme == 'dark' ? 'dark' : 'light');
             this.chartInstance.update()
+        },
+
+        setConfigurations() {
+            if (this.configuration.chart.type == 'radialBar') {
+                this.chartInstance.theme.w.config.plotOptions.radialBar.dataLabels.value.formatter = function(w, x) {
+                    return w + '';
+                };
+
+                this.chartInstance.theme.w.config.plotOptions.radialBar.dataLabels.total.formatter = function(w, x) {
+                    var sum = 0;
+
+                    w.globals.series.map((n) => sum += n);
+
+                    return sum + '';
+                };
+            }
+
+            this.chartInstance.theme.w.config.legend.formatter = function(w) {
+                return w;
+            };
+
+            if (['donut', 'pie'].includes(this.configuration.chart.type)) {
+                this.chartInstance.theme.w.config.dataLabels.formatter = function(w, x) {
+                    var index = x.seriesIndex;
+                    var serieValue = x.w.config.series[index];
+                    return serieValue + ' (' + Math.floor(w).toFixed(0) + '%)';
+                };
+            }
+
+            this.chartInstance.theme.w.config.title.style.fontFamily = 'Roboto';
+            this.chartInstance.theme.w.config.title.style.fontWeight = 'bold';
+            this.chartInstance.theme.w.config.yaxis[0].title.style.fontWeight = 'bold';
+            this.chartInstance.theme.w.config.xaxis.title.style.fontWeight = 'bold';
+
+            this.chartInstance.theme.w.config.legend.markers.offsetX = -3;
         },
 
         setColors(theme = null) {
@@ -32,23 +67,13 @@
 
             {{-- primary texts --}}
             var primaryText = this.texts[theme][0];
-
             this.chartInstance.theme.w.config.title.style.color = primaryText;
-            this.chartInstance.theme.w.config.title.style.fontFamily = 'Roboto';
-            this.chartInstance.theme.w.config.title.style.fontWeight = 'bold';
-
             this.chartInstance.theme.w.config.xaxis.title.style.color = primaryText;
-            this.chartInstance.theme.w.config.xaxis.title.style.fontWeight = 'bold';
-
             this.chartInstance.theme.w.config.yaxis[0].title.style.color = primaryText;
-            this.chartInstance.theme.w.config.yaxis[0].title.style.fontWeight = 'bold';
 
             {{-- secondary texts --}}
             var secondaryText = this.texts[theme][1];
-
-            this.chartInstance.theme.w.config.legend.markers.offsetX = -3;
             this.chartInstance.theme.w.config.legend.labels.colors = secondaryText;
-
             this.chartInstance.theme.w.config.xaxis.labels.style.colors = secondaryText;
             this.chartInstance.theme.w.config.yaxis[0].labels.style.colors = secondaryText;
         }
