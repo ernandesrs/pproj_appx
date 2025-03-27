8376a00ea48f8e6ff8546ba82f79a2c5d5721978
Ernandes R Souza <ernandesrsouza@gmail.com>
2025-03-27 18:23:12 -0400

    user fields name rmeoveclear

--- resources/views/livewire/shared/includes/user-basic-data.blade.php
+++ resources/views/livewire/shared/includes/user-basic-data.blade.php
@@ -7,28 +7,24 @@
     class="col-span-12 sm:col-span-6"
     type="text"
     label="Nome"
-    name="first_name"
     wire:model="{{ $formName }}.first_name" :error="$errors->first($formName . '.first_name')" />
 
 <x-shared.form.field
     class="col-span-12 sm:col-span-6"
     type="text"
     label="Sobrenome"
-    name="last_name"
     wire:model="{{ $formName }}.last_name" :error="$errors->first($formName . '.last_name')" />
 
 <x-shared.form.field
     class="col-span-12 sm:col-span-6"
     type="text"
     label="Usuário"
-    name="username"
     wire:model="{{ $formName }}.username" :error="$errors->first($formName . '.username')" />
 
 <x-shared.form.field
     class="col-span-12 sm:col-span-6"
     type="select"
     label="Gênero"
-    name="gender"
     wire:model='{{ $formName }}.gender'
     :options="collect(\App\Enums\UserGendersEnum::cases())
         ->map(fn($gender) => ['value' => $gender->value, 'label' => $gender->label()])
@@ -38,7 +34,6 @@
     class="col-span-12"
     type="email"
     label="E-mail"
-    name="email"
     :disabled="!$creating"
     wire:model="{{ $formName }}.email" :error="$errors->first($formName . '.email')" />
 
@@ -46,12 +41,10 @@
     class="col-span-12 sm:col-span-6"
     type="password"
     label="Senha"
-    name="password"
     wire:model="{{ $formName }}.password" :error="$errors->first($formName . '.password')" />
 
 <x-shared.form.field
     class="col-span-12 sm:col-span-6"
     type="password"
     label="Confirmar senha"
-    name="password_confirmation"
     wire:model="{{ $formName }}.password_confirmation" :error="$errors->first($formName . '.password_confirmation')" />
